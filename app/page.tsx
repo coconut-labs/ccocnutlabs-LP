6e2b6bae8971bf65e6d5ee72cf20d1867c622d60
Shrey Patel <patelshrey77@gmail.com>
2026-04-28 17:46:38 -0400

    refactor(home): swap Projects above Research (lead with the artifact, not the writing)

--- app/page.tsx
+++ app/page.tsx
@@ -11,8 +11,8 @@ export default function HomePage() {
     <>
       <Hero />
       <ManifestoStrip />
-      <ResearchStrip />
       <ProjectsStrip />
+      <ResearchStrip />
       <PeopleStrip />
       <ContactStrip />
       <LiveSignalsStrip />
